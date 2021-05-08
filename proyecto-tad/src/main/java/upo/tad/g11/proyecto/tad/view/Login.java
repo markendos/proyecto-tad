7d91474203e02d161e6c82fc91b8e5f6e1532ba4
Amalio <amalio@secmotic.com>
2021-05-08 14:31:11 +0200

    Creación de vista de Login

---
+++ proyecto-tad/src/main/java/upo/tad/g11/proyecto/tad/view/Login.java
@@ -0,0 +1,78 @@
+package upo.tad.g11.proyecto.tad.view;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.server.WrappedSession;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import upo.tad.g11.proyecto.tad.controller.ControladorPersonal;
+
+@Theme("mytheme")
+@Title("Login")
+public class Login extends UI {
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        //Creamos el layout
+        final VerticalLayout layout = new VerticalLayout();
+        //Layout de la pagina
+        layout.setMargin(true);
+
+        //Creamos la sesion
+        WrappedSession sesion = getSession().getSession();
+
+        //Formulario de inicio de sesion
+        TextField tfEmail = new TextField("Introduzca su correo para iniciar sesion");
+        TextField tfPass = new TextField("Introduzca su contraaseña para iniciar sesion");
+        Button submit = new Button("Guardar");
+
+        //Guardamos el formulario en el layout
+        layout.addComponent(tfEmail);
+        layout.addComponent(tfPass);
+        layout.addComponent(submit);
+
+        //Listener para guardar la sesion
+        submit.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                //Recogemos los valores del formulario
+                String email = tfEmail.getValue();
+                String pass = tfPass.getValue();
+
+                ControladorPersonal cp = new ControladorPersonal();
+                if (cp.Check(email, pass)) {
+
+                    //Guardamos en la sesion el email del usuario
+                    sesion.setAttribute("usuario", email);
+                    //Redireccionamos a la pagina MantenimientoDeEntidad
+                    UI.getCurrent().getPage().setLocation("./");
+
+                    System.out.println("\n\nSESION ACTIVADA\n\n");
+                }
+            }
+
+        });
+        //Comprobamos si el usuario tiene ya una sesion activa
+        if (sesion.getAttribute("usuario") != null) {
+            //UI.getCurrent().getPage().setLocation("./MantenimientoDeEntidad");
+            System.out.println("\n\nSESION ACTIVA\n\n");
+        }
+
+        setContent(layout);
+    }
+
+    @WebServlet(value = {"/*", "/VAADIN/*"}, name = "Login", asyncSupported = true)
+    @VaadinServletConfiguration(productionMode = false, ui = Login.class)
+    public static class Servlet extends VaadinServlet {
+    }
+}
