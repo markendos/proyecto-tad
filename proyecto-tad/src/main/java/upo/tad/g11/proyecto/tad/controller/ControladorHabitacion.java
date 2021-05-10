b385622853c13e7809a8df3870da79b4e5af8528
Amalio <amaliocabeza.16@gmail.com>
2021-05-10 19:40:41 +0200

    Creacion controladores

---
+++ proyecto-tad/src/main/java/upo/tad/g11/proyecto/tad/controller/ControladorHabitacion.java
@@ -0,0 +1,63 @@
+package upo.tad.g11.proyecto.tad.controller;
+
+import java.util.List;
+import upo.tad.g11.proyecto.tad.model.DAO.DAO;
+import upo.tad.g11.proyecto.tad.model.DAO.HabitacionDAO;
+import upo.tad.g11.proyecto.tad.model.entity.Habitacion;
+
+public class ControladorHabitacion implements Controlador<Habitacion> {
+    
+    //Definicion de los atributos
+    private DAO<Habitacion> habitaciones = new HabitacionDAO();
+
+    /**
+     * Metodo que formatea los datos y prepara un nuevo objeto que sera agregado
+     * en la BD
+     *
+     */
+    
+    public void add(Habitacion t) {
+        if (habitaciones.get(t.getId()) == null) {
+            this.habitaciones.save(t);
+        }
+    }
+
+    /**
+     * Metodo que formatea los datos y prepara un nuevo objeto que sera
+     * actualizado en la BD
+     *
+     */
+    public void update(Habitacion t) {
+        this.habitaciones.save(t);
+
+    }
+
+    /**
+     * Metodo que recibe un id y lo elimina de la BD
+     *
+     */
+    public void delete(Habitacion t) {
+        
+        this.habitaciones.delete(t);
+    }
+
+    /**
+     * Metodo que pide un id al DAO y lo devuelve a la pantalla principal
+     *
+     */
+    public Habitacion get(Habitacion t) {
+        Habitacion c = habitaciones.get(t.getId());
+        return c;
+    }
+
+    /**
+     * Metodo que pide al DAO todos los objetos DTO para mostrarselos al usuario
+     * como lista
+     *
+     */
+    @Override
+    public List listar() {
+        return habitaciones.getAll();
+    }
+
+}
